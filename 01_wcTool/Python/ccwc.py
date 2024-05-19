570adf63529621648abb0fcf12f9e112217b172f
Rakesh <ec2-user@ip-172-31-17-182.eu-west-1.compute.internal>
2024-05-19 10:47:03 +0000

    Added Steps 1,2,3,5

--- 01_wcTool/Python/ccwc.py
+++ 01_wcTool/Python/ccwc.py
@@ -1,23 +1,94 @@
 #Reading from a file
 
-import getopt, sys, os
+import getopt, sys, os, string
 
-argList = sys.argv[1:]
-# print(argList)
-options = "c:"
+def get_bytes_in_file(file):
+    fileReader = open(file, "rb")   
+    byte=0
+    whitespaces = [" ", "\t", "\n", "\v", "\r", "\f"]
+    while 1:
+        char = fileReader.read(1)
+        if not char: 
+            break
 
-longOptions = ["character"]
+        if (char in whitespaces or char):
+            byte+=1
 
-try:
-    arguments, values = getopt.getopt(argList, options, longOptions)
+    fileReader.close()
+    return byte
 
-    for currentArg, currentValue in arguments:
-        if currentArg in ("-c", "--Character"):
-            
-            fileReader = open("text.txt", "r")
-            print(str(os.path.getsize("text.txt")) + " " + argList[1])
-            fileReader.close()
+def get_lines_in_file(file):
+    fileReader = open(file, "rb")
+    line = 0
+    while(fileReader.readline()):
+        line += 1
+
+    fileReader.close()
+    return line
+
+def get_words_in_file(file):
+    fileReader = open(file, "rb")
+    words = 0
+    while 1:
+        line = fileReader.readline()
+        words += len(line.split())
+        if not line:
+            break
+
+    fileReader.close()
+    return words
+
+def get_chars_in_file(file):
+    fileReader = open(file, "rb")
+    chars = 0
+    whitespaces = [" ", "\t", "\n", "\v", "\r", "\f"]
+    while 1:
+        char = fileReader.read(1)
+        if char not in whitespaces:
+            chars +=1
+        if not char: 
+            break
+    fileReader.close()
+    return chars
+
+def help():
+    print('Coding Challenge wc Tool')
+    print('Usage: python3 ccwc.py [OPTIONS] [FILE]')
+    print('Options:')
+    print('  -c, --bytes\n\tprint the number of bytes')
+    print('  -l, --lines\n\tprint the number of lines')
+    print('  -w, --words\n\tprint the number of word')
+    print('  -m, --chars\n\tprint the number of chars')
 
 
-except getopt.error as err:
-    print(str(err))
+if __name__ == "__main__" :
+    argList = sys.argv[1:]
+    options = "clwm"
+    longOptions = ["bytes", "lines", "words", "chars"]       
+    # print(argList)
+    if len(argList) == 0:
+        help()
+    elif len(argList) == 1 and argList[0] not in options:  
+        print(str(get_lines_in_file(argList[0])) + " " + str(get_words_in_file(argList[0])) + " " +  str(get_bytes_in_file(argList[0])) + " " + argList[0])
+
+    else:
+        try:
+            arguments, values = getopt.getopt(argList, options, longOptions)
+
+            for currentArg, currentValue in arguments:
+                # print(arguments)
+                if currentArg in ("-c", "--bytes"):
+                    print(str(get_bytes_in_file(argList[1])) + " " + argList[1])
+                elif currentArg in ("-l", "--lines"):
+                    print(str(get_lines_in_file(argList[1])) + " " + argList[1])                 
+                elif currentArg in ("-w", "--words"):
+                    print(str(get_words_in_file(argList[1])) + " " + argList[1])                 
+                elif currentArg in ("-m", "--chars"):
+                    print(str(get_chars_in_file(argList[1])) + " " + argList[1])  
+                else:
+                    print('Usage: python3 ccwc.py [OPTIONS] file')
+
+
+
+        except getopt.error as err:
+            print(str(err))
--- 01_wcTool/README.md
+++ 01_wcTool/README.md
@@ -3,3 +3,8 @@
 This Challenge is to build your own **wc** tool.
 
 Adding all the references I used to build this tool.
+
+**Python References**
+1. Reading arguments from Command Line: [Command Line Arguments in Python](https://www.geeksforgeeks.org/command-line-arguments-in-python/)
+2. Reading from a File: [Python File Open](https://www.w3schools.com/python/python_file_open.asp)
+3. Python Documentation: [Reading and Writing Files](https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files)
